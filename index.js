30da32757e3b418c782d9e824cdb8456d5eabad3
doowb <brian.woodward@gmail.com>
2017-04-10 11:33:01 -0400

    implementation

--- index.js
+++ index.js
@@ -1,4 +1,30 @@
 'use strict';
 
-module.exports = function() {
-};
+function sma(arr, range, format) {
+  var fn = typeof format === 'function' ? format : toFixed;
+  var num = range || arr.length;
+  var res = [];
+  var len = arr.length + 1;
+  var idx = num - 1;
+  while (++idx < len) {
+    res.push(fn(avg(arr, idx, idx < num ? idx : num)));
+  }
+  return res;
+}
+
+function avg(arr, idx, range) {
+  return sum(arr.slice(idx - range, idx)) / range;
+}
+
+function sum(arr) {
+  var len = arr.length;
+  var num = 0;
+  while (len--) num += Number(arr[len]) >> 0;
+  return num;
+}
+
+function toFixed(n) {
+  return n.toFixed(2);
+}
+
+module.exports = sma;
